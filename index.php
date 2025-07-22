8bde2b4eb1cdb8369562dbb70e7d4e229f3bf753
Kajal Kupale <kajalkupale25@gmail.com>
2025-07-22 18:40:57 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include "layouts/header.php"; ?>
+<div class="container">
+
+<center><h2 style="color:#2196f3; box-shadow: 0 0 10px #2196f3, 0 0 40px #2196f3, 0 0 80px #2196f3; text-shadow: 0 0 10px #ecadfd, 0 0 40px #ecadfd, 0 0 80px #ecadfd; margin-top:20%"><b>WELCOME TO GLOBAL CHAT SYSTEM !!</b></h2></center>
+</div>
+
+</body>
+</html>
